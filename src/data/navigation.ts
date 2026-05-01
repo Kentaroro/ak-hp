418cbc869e1137f780002f7c0782998aad14b98a
kentarou <kentarou.suzuki.0829@gmail.com>
2026-05-02 05:54:38 +0900

    初回コミット

---
+++ src/data/navigation.ts
@@ -0,0 +1,12 @@
+export const navigation = [
+    { label: 'Home', href: '#home' },
+    { label: 'BIOGRAPHY', href: '#about' },
+    { label: 'DISCOGRAPHY', href: '#skills' },
+    { label: 'NEWS', href: '#contact' },
+    { label: 'CONTACT', href: '#works' },
+]
+
+export const sns = [
+    { label: 'Twitter', href: 'https://x.com/home' },
+    { label: 'Youtube', href: 'https://www.youtube.com/' },
+]
